dfb41a6ab40b94f7fc85707b8fcf9b27ef4ae595
Diego Carnero <diegocarnero@users.noreply.github.com>
2025-11-03 12:29:29 +0100

    Fix improper demand handling in Broadcaster. Add test

--- apps/pipeline/lib/pipeline/stages/broadcaster.ex
+++ apps/pipeline/lib/pipeline/stages/broadcaster.ex
@@ -8,8 +8,10 @@ defmodule Pipeline.Stages.Broadcaster do
   def init(initial) do
     state = %{pending_demand: 0, buffer: []}
     subscribe_options = Map.get(initial, :subscribe_options, nil)
+
     if subscribe_options do
-      {:producer_consumer, state, subscribe_to: subscribe_options, dispatcher: GenStage.BroadcastDispatcher}
+      {:producer_consumer, state,
+       subscribe_to: subscribe_options, dispatcher: GenStage.BroadcastDispatcher}
     else
       {:producer, state, dispatcher: GenStage.BroadcastDispatcher}
     end
@@ -22,6 +24,7 @@ defmodule Pipeline.Stages.Broadcaster do
     next_state =
       state
       |> Map.put(:buffer, remaining)
+      |> Map.put(:pending_demand, pending_demand - length(to_emit))
 
     {to_emit, next_state}
   end
@@ -29,26 +32,22 @@ defmodule Pipeline.Stages.Broadcaster do
   def handle_demand(new_demand, state) do
     pending_demand = state.pending_demand
     demand = pending_demand + new_demand
-    state = %{ state | pending_demand: demand}
+    state = %{state | pending_demand: demand}
     {to_emit, next_state} = get_events_to_emit_and_next_state(state)
 
     {:noreply, to_emit, next_state}
   end
 
-  def handle_info(new_events, %{ pending_demand: 0 } = old_state) do
-    buffer = old_state.buffer
-    new_state = %{ old_state | buffer: buffer ++ new_events}
-    {:noreply, [], new_state}
-  end
+  def handle_info(events, state) when is_list(events), do: handle_events(events, self(), state)
 
-  def handle_info(new_events, %{ pending_demand: pending_demand } = old_state) do
-    buffer = old_state.buffer
-    new_state = %{ old_state | buffer: buffer ++ new_events}
+  def handle_info(event, state), do: handle_events([event], self(), state)
+
+  def handle_events(new_events, _from, state) do
+    buffer = state.buffer
+    new_state = %{state | buffer: buffer ++ new_events}
     {to_emit, next_state} = get_events_to_emit_and_next_state(new_state)
-    next_state =
-      next_state
-      |> Map.put(:pending_demand, pending_demand - length(to_emit))
 
     {:noreply, to_emit, next_state}
   end
+
 end
---
+++ apps/pipeline/test/stages/broadcaster_test.exs
@@ -0,0 +1,51 @@
+defmodule BroadcasterTest do
+  use ExUnit.Case
+  doctest Pipeline
+
+  defmodule Forwarder do
+  use GenStage
+
+    # Starts the consumer process
+    def start_link(%{name: name, parent: parent, subscribe_to: subscribe_to}) do
+      GenStage.start_link(__MODULE__, %{parent: parent, subscribe_to: subscribe_to}, name: name)
+    end
+
+    def init(%{parent: parent, subscribe_to: subscribe_opts}) do
+      {:consumer, parent, subscribe_to: subscribe_opts}
+    end
+
+    def handle_events(events, _from, parent) do
+      send(parent, {self(), events})
+      {:noreply, [], parent}
+    end
+  end
+
+  test "broadcaster" do
+    # arrange
+    proc_under_test_name = :broadcaster_test
+    subscribe_opts = [{proc_under_test_name, max_demand: 10}]
+
+    {:ok, bc_pid} = Pipeline.Stages.Broadcaster.start_link(%{name: proc_under_test_name})
+    {:ok, fwd_1_pid} = Forwarder.start_link(%{name: :fwd_1, parent: self(), subscribe_to: subscribe_opts})
+    {:ok, fwd_2_pid} = Forwarder.start_link(%{name: :fwd_2, parent: self(), subscribe_to: subscribe_opts})
+
+    # act
+    send(bc_pid, Enum.to_list(1..11))
+    # assert
+    assert_receive {^fwd_1_pid, [1, 2, 3, 4, 5]}
+    assert_receive {^fwd_2_pid, [1, 2, 3, 4, 5]}
+    assert_receive {^fwd_1_pid, [6, 7, 8, 9, 10]}
+    assert_receive {^fwd_2_pid, [6, 7, 8, 9, 10]}
+    assert_receive {^fwd_1_pid, [11]}
+    assert_receive {^fwd_2_pid, [11]}
+    Process.sleep(10)
+    assert %{state: %{pending_demand: 9}} = :sys.get_state(bc_pid)
+    # clean
+    Process.unlink(fwd_1_pid)
+    Process.exit(fwd_1_pid, :kill)
+    Process.unlink(fwd_2_pid)
+    Process.exit(fwd_2_pid, :kill)
+    Process.unlink(bc_pid)
+    Process.exit(bc_pid, :kill)
+  end
+end
