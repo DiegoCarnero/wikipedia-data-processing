d50061334c0eac8fc580be81796005ba34bb8c80
Diego Carnero <diegocarnero@users.noreply.github.com>
2025-11-28 23:41:17 +0100

    Fix duplicates in Flow output

--- apps/transformations/lib/flows/flows.ex
+++ apps/transformations/lib/flows/flows.ex
@@ -16,11 +16,33 @@ defmodule Transformations.Flows do
           results =
             Transformations.aggr_grouped(acc)
             |> Enum.map(&Map.put(&1, :interval, window))
-          {results, acc}
+          {results, []}
         else
           {[], acc}
         end
       end)
   end
 
+  def from_local_ndjson(raw_ndjson_filepath) do
+    File.stream!(raw_ndjson_filepath)
+    |> Flow.from_enumerable()
+    |> Flow.map(fn line ->
+      case Jason.decode(line) do
+          {:ok, map} -> map
+          {:error, _reason} -> %{}
+        end
+    end)
+  end
+
+  def from_local_ndjson_data_only(raw_ndjson_filepath) do
+    Transformations.Flows.from_local_ndjson(raw_ndjson_filepath)
+    |> Flow.filter(fn map -> Map.has_key?(map, "data") end)
+    |> Flow.map(fn map ->
+      data_str = Access.get(map, "data")
+      case Jason.decode(data_str) do
+          {:ok, data} -> data
+          {:error, _reason} -> %{}
+        end
+    end)
+  end
 end
